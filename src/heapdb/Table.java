a3fbffd3d9762b8a5db80de0279c902376bf7d98
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-02-07 18:16:15 -0800

    add: lookup method part2 which has 2 parameter colname and value

--- src/heapdb/Table.java
+++ src/heapdb/Table.java
@@ -5,158 +5,193 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Table implements ITable {
-	
-	private List<Tuple> tuples;
-	private Schema schema;
-	
-	public Table(Schema schema) {
-		this.schema = schema;
-		tuples = new ArrayList<>();
-	}
 
-	public static void main(String[] args) {
-		Schema schema = new Schema();
-		schema.addKeyIntType("ID");   // primary key ID int
-		schema.addVarCharType("name");
-		schema.addVarCharType("dept_name");
-		schema.addIntType("salary");
-		Tuple[] tuples = new Tuple[] {
-				new Tuple(schema, 22222, "Einstein",    "Physics", 95000),
-				new Tuple(schema, 12121, "Wu",          "Finance", 90000),
-				new Tuple(schema, 32343, "El Said" ,    "History", 60000),
-				new Tuple(schema, 45565, "Katz",        "Comp. Sci.", 75000),
-				new Tuple(schema, 98345, "Kim",         "Elec. Eng.", 80000),
-				new Tuple(schema, 10101, "Srinivasan" , "Comp. Sci.", 65000),
-				new Tuple(schema, 76766, "Crick" ,      "Biology", 72000),
-		};
-		Table table = new Table(schema);
-		for (int i = 0; i < tuples.length; i++) {
-			table.insert(tuples[i]);
-		}
-		System.out.println(table.toString());
+    private List<Tuple> tuples;
+    private Schema schema;
 
-		Tuple oldTup = new Tuple(schema, 22222, "Einstein",    "Physics", 95000 );
-		table.insert(oldTup);
-		System.out.println(table.toString());
+    public Table(Schema schema) {
+        this.schema = schema;
+        tuples = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Schema schema = new Schema();
+        schema.addKeyIntType("ID");   // primary key ID int
+        schema.addVarCharType("name");
+        schema.addVarCharType("dept_name");
+        schema.addIntType("salary");
+        Tuple[] tuples = new Tuple[]{
+                new Tuple(schema, 22222, "Einstein", "Physics", 95000),
+                new Tuple(schema, 12121, "Wu", "Finance", 90000),
+                new Tuple(schema, 32343, "El Said", "History", 60000),
+                new Tuple(schema, 45565, "Katz", "Comp. Sci.", 75000),
+                new Tuple(schema, 98345, "Kim", "Elec. Eng.", 80000),
+                new Tuple(schema, 10101, "Srinivasan", "Comp. Sci.", 65000),
+                new Tuple(schema, 76766, "Crick", "Biology", 72000),
+        };
+        Table table = new Table(schema);
+        for (int i = 0; i < tuples.length; i++) {
+            table.insert(tuples[i]);
+        }
+        System.out.println(table.toString());
+
+        Tuple oldTup = new Tuple(schema, 22222, "Einstein", "Physics", 95000);
+        table.insert(oldTup);
+        System.out.println(table.toString());
 //		Tuple newTup = new Tuple(schema, 11111, "Molina",      "Music",   70000 );
+        int ID = oldTup.getInt(0);
+        System.out.println(ID);
 
-		System.out.println(table.lookup(22222));
-		System.out.println(table.lookup(11111));
+//		System.out.println(tuples[0].getSchema().getName(0));
+        System.out.println(table.lookup("ID", 12121));
+//		System.out.println(table.lookup(11111));
 
-	}
-	
-	@Override
-	public Schema getSchema() {
-		return schema;
-	}
+//		ITable t = new Table(schema);
+//		t.insert
+//		System.out.print(t);
 
-	
-	@Override
-	public int size() {
-		return tuples.size();
-	}
+    }
 
-	@Override
-	public void close() {
-		// do nothing
-	}
-	
-	@Override
-	public boolean insert(Tuple rec) {
-		if (! rec.getSchema().equals(schema)) {
-			throw new IllegalStateException("Error: tuple schema does not match table schema.");
-		}
-		for (Tuple tuple: tuples) {
-			if (tuple.getKey().equals(rec.getKey())) {
-				return false;
-			}
-		}
-		tuples.add(rec);
-		return true;
-	}
-
-	@Override
-	public boolean delete(Object key) {
-		if (schema.getKey() == null) {
-			throw new IllegalStateException("Error: table does not have a primary key.  Can not delete.");
-		}
-		for (Tuple tuple: tuples) {
-			if (tuple.getKey().equals(key)) {
-				tuples.remove(tuple);
-				return true;
-			}
-		}
-		return false;
-	}
+    @Override
+    public Schema getSchema() {
+        return schema;
+    }
 
 
-	@Override
-	public Tuple lookup(Object key) {
-		if (schema.getKey() == null) {
-			throw new IllegalStateException("Error: table does not have a primary key.  Can not lookup by key.");
-		}
+    @Override
+    public int size() {
+        return tuples.size();
+    }
 
-		for (Tuple tuple: tuples) {
-			if (tuple.getKey().equals(key)) {
-				return tuple;
-			}
-		}
-		return null;
-	}
+    @Override
+    public void close() {
+        // do nothing
+    }
 
-	@Override
-	public ITable lookup(String colname, Object value) {
-		if (schema.getColumnIndex(colname) < 0) {
-			throw new IllegalStateException("Error: table does not contain column "+colname);
-		}
+    @Override
+    public boolean insert(Tuple rec) {
+        if (!rec.getSchema().equals(schema)) {
+            throw new IllegalStateException("Error: tuple schema does not match table schema.");
+        }
+        for (Tuple tuple : tuples) {
+            if (tuple.getKey().equals(rec.getKey())) {
+                return false;
+            }
+        }
+        tuples.add(rec);
+        return true;
+    }
 
-		// TODO implement this method
-		
-		throw new  UnsupportedOperationException();
+    @Override
+    public boolean delete(Object key) {
+        if (schema.getKey() == null) {
+            throw new IllegalStateException("Error: table does not have a primary key.  Can not delete.");
+        }
+        for (Tuple tuple : tuples) {
+            if (tuple.getKey().equals(key)) {
+                tuples.remove(tuple);
+                return true;
+            }
+        }
+        return false;
+    }
 
-	}
 
-	@Override
-	public Iterator<Tuple> iterator() {
-		return new TIterator(tuples.iterator());
-	}
-	
-	@Override
-	public String toString() {
-		
-		// TODO implement this method
-		if (tuples.isEmpty()) {
-			return "Empty Table";
-		} else {
-			StringBuilder sb = new StringBuilder();
-			for (Tuple t : this) {
-				sb.append(t.toString());
-				sb.append("\n");
-			}
-			return sb.toString();
-		}
-	}
-	
-	/*
-	 * An iterator that returns a copy of each tuple in 
-	 * the table.
-	 */
-	public static class TIterator implements Iterator<Tuple> {
-		
-		private Iterator<Tuple> it;
-		
-		public TIterator(Iterator<Tuple> it) {
-			this.it = it;
-		}
+    @Override
+    public Tuple lookup(Object key) {
+        if (schema.getKey() == null) {
+            throw new IllegalStateException("Error: table does not have a primary key.  Can not lookup by key.");
+        }
 
-		@Override
-		public boolean hasNext() {
-			return it.hasNext();
-		}
+        for (Tuple tuple : tuples) {
+            if (tuple.getKey().equals(key)) {
+                return tuple;
+            }
+        }
+        return null;
+    }
 
-		@Override
-		public Tuple next() {
-			return new Tuple(it.next());
-		}	
-	}
+    @Override
+    public ITable lookup(String colname, Object value) {
+        if (schema.getColumnIndex(colname) < 0) {
+            throw new IllegalStateException("Error: table does not contain column " + colname);
+        }
+        // TODO implement this method
+        ITable t = new Table(schema);
+        for (int i = 0; i < tuples.size(); i++) {
+            if (tuples.get(i).getSchema().getName(i).equals(colname)) {
+                Tuple tuple = tuples.get(i);
+                switch (colname) {
+                    case "ID":
+                        if (tuple.get(0).equals(value)) {
+                            t.insert(tuple);
+                        }
+                        break;
+                    case "name":
+                        if (tuple.get(1).equals(value)) {
+                            t.insert(tuple);
+                        }
+                        break;
+                    case "dept_name":
+                        if (tuple.get(2).equals(value)) {
+                            t.insert(tuple);
+                        }
+                        break;
+                    case "salary":
+                        if (tuple.get(3).equals(value)) {
+                            t.insert(tuple);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                return t;
+            }
+        }
+        return t;
+    }
+
+    @Override
+    public Iterator<Tuple> iterator() {
+        return new TIterator(tuples.iterator());
+    }
+
+    @Override
+    public String toString() {
+
+        // TODO implement this method
+        if (tuples.isEmpty()) {
+            return "Empty Table";
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (Tuple t : this) {
+                sb.append(t.toString());
+                sb.append("\n");
+            }
+            return sb.toString();
+        }
+    }
+
+    /*
+     * An iterator that returns a copy of each tuple in
+     * the table.
+     */
+    public static class TIterator implements Iterator<Tuple> {
+
+        private Iterator<Tuple> it;
+
+        public TIterator(Iterator<Tuple> it) {
+            this.it = it;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return it.hasNext();
+        }
+
+        @Override
+        public Tuple next() {
+            return new Tuple(it.next());
+        }
+    }
 }
