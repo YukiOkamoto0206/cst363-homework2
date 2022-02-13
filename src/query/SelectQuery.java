1132ed13d7052646b34244f6c1ee6c5eecf65ba4
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-02-13 14:50:13 -0800

    add: SelectQuery for simpleStrQueryTest

--- src/query/SelectQuery.java
+++ src/query/SelectQuery.java
@@ -2,7 +2,9 @@ package query;
 
 
 import heapdb.ITable;
+import heapdb.Schema;
 import heapdb.Table;
+import heapdb.Tuple;
 
 /**
  * A simple select query of the form:
@@ -45,16 +47,29 @@ public class SelectQuery  {
 	
 	public ITable eval(ITable table) {
 		// TODO replace with your code.
-
 		// 1. create a result Schema if colNames is not null. Use Schema.project method.
+		if (colNames != null) {
+//			Schema schema = new Schema();
+//			schema.project(colNames);
+			table.getSchema().project(colNames);
+		}
 		// 2. create a result Table.
+		ITable result_table = new Table(table.getSchema());
 		// 3. Iterate over all tuple in table given as argument.
+//		for (int i = 0; i< result_table.size(); i++) {
+//			result_table.
+//		}
 		// 4.		for each row,  cond.eval(tuple)
 		// 5. 		if false skip to next row.
 		// 6.			if ture --> project columns from tuple, using Tuple.project ethod
 		// 				insert into result table
 		// 7. return result table.
-		throw new  UnsupportedOperationException();
+		for (Tuple tuple : table) {
+			if (cond.eval(tuple)) {
+				result_table.insert(tuple);
+			}
+		}
+		return result_table;
 	}
 
 	@Override
